5f9e57e7400c27c4c7f898fc624dd583bc8108ef
Marian <marian@marian.(none)>
2012-12-30 21:12:06 +0200

    cpp 2

--- First/main.cpp
+++ First/main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 
 using namespace std;
-
+void afiseaza ();
 int main()
 {
+    int x = 5;
+    int *adr = &x;
+    afiseaza();
     cout << "Hello world!" << endl;
     cout << "test" << endl;
+    cout << adr << endl;
     return 0;
+}
+void afiseaza (){
+    cout <<"1"<<endl;
 }
